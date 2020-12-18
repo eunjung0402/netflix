05a5d675f05a2098d2e950b452ea64f1d71bd8c5
eunjung0402 <76139130+eunjung0402@users.noreply.github.com>
2020-12-18 19:14:32 +0900

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,11 @@
+$(function(){
+   //자주묻는 질문
+    
+    $('.sect4 li').click(function(){
+        $(this).find('.a').slideToggle();
+//        $(this).find('.icon').addClass('ro');
+    });
+    
+    
+    
+});
