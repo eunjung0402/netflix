6a853a9e94b175028b7b821c51286d7d4bf27438
eunjung0402 <76139130+eunjung0402@users.noreply.github.com>
2020-12-21 10:58:28 +0900

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -1,11 +1,14 @@
 $(function(){
    //자주묻는 질문
     
-    $('.sect4 li').click(function(){
-        $(this).find('.a').slideToggle();
-//        $(this).find('.icon').addClass('ro');
+        /*var sta=false; */   //상태는 시작하자마자 안보이게 상태 저장
+        $('.q').click(function(){
+        $('.a').hide();
+        $(this).next('.a').slideDown();
+        $('.icon').removeClass('ro');
+        $(this).siblings('.icon').addClass('ro');
+        
+    });
+              
     });
     
-    
-    
-});
